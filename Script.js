2fb422060cae0e9b9d1f619d57eaa574c8b65ac1
S <samtonycooper@gmail.com>
2024-02-13 18:53:03 +0000

    Minor Reworks

--- Script.js
+++ Script.js
@@ -8,3 +8,17 @@ function ToggleDark() {
         button.innerHTML = "Dark Mode"
     }
 }
+
+function CloseMenu() {
+    document.getElementById("Sidebar").style.display = "none";
+    //document.getElementById("Main").style.marginLeft = "0%";
+    //document.getElementById("Sidebar").style.marginLeft = "0%";
+    document.getElementById("ToggleMenu").onclick = OpenMenu;
+}
+
+function OpenMenu() {
+    document.getElementById("Sidebar").style.display = "inline";
+    //document.getElementById("Main").style.marginLeft = "10%";
+    document.getElementById("ToggleMenu").innerHTML = "x";
+    document.getElementById("ToggleMenu").onclick = CloseMenu;
+}
